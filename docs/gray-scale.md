04308679b74d1eb2760bb031a2ed7a0a6ed6c5b1
wasuken <wasuken1@gmail.com>
2020-11-17 11:47:19 +0900

    impl gray-scale.

---
+++ docs/gray-scale.md
@@ -0,0 +1,7 @@
+# gray-scale memo
+
+```(R+G+B) / 3```で算出するらしい。楽だぁ。
+
+## 参考URL
+
+[グレースケール変換 - 【ゆるゆるプログラミング】](https://talavax.com/grayscale.html)
--- src/main.lisp
+++ src/main.lisp
@@ -1,6 +1,7 @@
 (defpackage cl-image-hash
   (:use :cl :opticl)
-  (:export :ahash-gen-hash :dhash-gen-hash :hash-dim-bit-diff))
+  (:export :ahash-gen-hash :dhash-gen-hash :hash-dim-bit-diff
+		   :hash-dim-bit-match-persent))
 (in-package :cl-image-hash)
 
 (defun flatten (obj)
@@ -13,11 +14,76 @@
 		 '())
 		(t `(,obj))))
 
+(defun gray-bytes->list (two-d-bytes)
+  (loop for i below (array-dimension two-d-bytes 0)
+	 collect (loop for j below (array-dimension two-d-bytes 1)
+				collect (aref two-d-bytes i j))))
+
+(defun color-bytes->list (three-d-bytes)
+  (loop for i below (array-dimension three-d-bytes 0)
+	 collect (loop for j below (array-dimension three-d-bytes 1)
+				collect (loop for k below (array-dimension three-d-bytes 2)
+						   collect (aref three-d-bytes i j k)))))
+
+(defun color-list->gray-scale (color-list)
+  (mapcar #'(lambda (x)
+			  (mapcar #'(lambda (y)
+						  (ceiling (float (/ (reduce #'+ y :initial-value 0)
+											 (length y)))))
+					  x))
+		  color-list))
+
+(defun byte-2d-list->array (byte-list byte-size)
+  (let ((result (make-array `(,(length byte-list)
+							   ,(length (car byte-list)))
+							:element-type `(unsigned-byte ,byte-size)
+							:initial-element 0)))
+	(loop for i below (length byte-list)
+	   for x in byte-list
+	   do (loop for j below (length (car byte-list))
+			 for y in x
+			 do (setf (aref result i j) y)))
+	result))
+
+(defun byte-3d-list->array (byte-list byte-size)
+  (let ((result (make-array `(,(length byte-list)
+							   ,(length (car byte-list))
+							   ,(length (car (car byte-list))))
+							:element-type `(unsigned-byte ,byte-size)
+							:initial-element 0)))
+	(loop for i below (length byte-list)
+	   for x in byte-list
+	   do (loop for j below (length (car byte-list))
+			 for y in x
+			 do (loop for k below (length (car (car byte-list)))
+				   for z in y
+				   do (setf (aref result i j k)
+							z))))
+	result))
+
+(defun byte-list->array (byte-list &optional (byte-size 8))
+  (cond ((and (eq (type-of byte-list) 'CONS)
+			  (eq (type-of (car byte-list)) 'CONS)
+			  (eq (type-of (car (car byte-list))) 'CONS))
+		 (byte-3d-list->array byte-list byte-size))
+		((and (eq (type-of byte-list) 'CONS)
+			  (eq (type-of (car byte-list)) 'CONS))
+		 (byte-2d-list->array byte-list byte-size))
+		(t (error "Not 2-3d byte-list"))))
+
 (defun png-file->dim-ary (path)
   (let ((img-bytes (opticl:read-png-file path)))
-	(loop for i below (array-dimension img-bytes 0)
-	   collect (loop for j below (array-dimension img-bytes 1)
-				  collect (aref img-bytes i j)))))
+	(cond ((eq (car (type-of img-bytes)) 'SIMPLE-ARRAY)
+		   (let ((dim (length (nth 2 (type-of img-bytes)))))
+			 (cond ((= 2 dim) (values (gray-bytes->list img-bytes) 'gray))
+				   ((= 3 dim) (values (color-bytes->list img-bytes) 'color))
+				   (t (error (format nil "{~S} bytes not support" path))))))
+		  (t (error (format nil "{~S} read data is not n-d array" path))))))
+
+(defun dim-ary->png-file (i-path o-path bytes)
+  (multiple-value-bind (bytes type)
+		(png-file->dim-ary i-path)
+	))
 
 (defun hash-dim-bit-diff (f a-path b-path)
   (let ((a-hash (funcall f a-path))
@@ -29,6 +95,9 @@
 			(unless (= a b)
 			  (incf cnt))))
 	cnt))
+
+(defun hash-dim-bit-match-persent (f a-path b-path &optional (len 64))
+  (ceiling (* 100 (float (/ (- len (hash-dim-bit-diff f a-path b-path)) len)))))
 
 (defun ahash-gen-hash (path)
   (let* ((img-byte-ary-ary (png-file->dim-ary path))
